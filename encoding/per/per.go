62b42ae5caff4c9a9d0612b658013263e1cabcfe
horai <horai@localhost.localdomain>
2019-10-18 17:34:14 +0000

    [update] add license

--- encoding/per/per.go
+++ encoding/per/per.go
@@ -1,3 +1,7 @@
+// Copyright 2019 hhorai. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found
+// in the LICENSE file.
+
 // Package per is implementation for Basic Pckage Encoding Rule (PER) in
 // ALIGNED variant.
 package per
